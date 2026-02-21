edc0d762afe4afa4420a4e71a03d2b00216ef46b
Mucyobruce <cboy85096@email.com>
2026-02-21 22:56:50 +0200

    changes

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,30 +1,60 @@
 import React, { useEffect, useState } from 'react'
 import { backendUrl } from '../App'
 import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const List = () => {
 
-const [list,setList]=useState([])
+  const [list, setList] = useState([])
 
-const fetchList=async()=>{
+  const fetchList = async () => {
 
-  try {
-    const responce=await axios.get(backendUrl + '/api/products/list')
-    console.log(responce)
-  } catch (error) {
-    
+    try {
+      const responce = await axios.get(backendUrl + '/api/products/list')
+      if (responce.data.success) {
+        setList(responce.data.allProducts)
+      }
+      else {
+        toast.error(responce.data.message)
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+
   }
 
-}
-
-useEffect(()=>{
-fetchList()  
-},[])
+  useEffect(() => {
+    fetchList()
+  }, [])
 
   return (
-    <div>
+    <>
+      <p className='mb-2'>All Products List</p>
+      <div className='flex flex-col gap-2'>
+        {/* List table title */}
 
+       <div className='hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm'>
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>price</b>
+          <b className='text-center'>Action</b>
+        </div>
+{/* All products  */}
+
+{
+  list.map((item,index)=>(
+    <div key={index}>
+<img src={item.image[0]} alt="" />
+<p>{item.name}</p>
+<p>{item.category}</p>
+<p>{item.price}</p>
     </div>
+  ))
+}
+      </div>
+    </>
   )
 }
 
